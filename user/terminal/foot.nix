4fab5870f1c0d84dc60315998906604345217e7f
jtawari <jtawari.me@gmail.com>
2024-11-18 18:57:21 +0530

    dotfiles: user: foot: Set font to Source Code Pro Nerd Font

--- user/terminal/foot.nix
+++ user/terminal/foot.nix
@@ -1,10 +1,23 @@
-{
+let
+  # Font family
+  font = "SauceCodePro Nerd Font";
+
+  # Font size
+  size = "9.4";
+in {
   # Configure Foot
   programs.foot = {
     enable = true;
     settings = {
       main = {
         term = "xterm-256color";
+
+        font = "${font}:size=${size}:style=SemiBold";
+        font-bold = "${font}:size=${size}:style=Bold";
+        font-italic = "${font}:size=${size}:style=Italic";
+        font-bold-italic = "${font}:size=${size}:style=Bold,Italic";
+        line-height = 17;
+        underline-offset = 17;
       };
     };
   };
